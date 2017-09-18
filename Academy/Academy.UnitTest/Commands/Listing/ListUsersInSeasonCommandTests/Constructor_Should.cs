f2b29dfe0ff590131ce8fdc44c7e23570e589ccc
SvetozarMateev <vidic9898@gmail.com>
2017-09-19 00:27:06 +0300

    added unit test for ListUsersInSeasonCommandTests

---
+++ Academy/Academy.UnitTest/Commands/Listing/ListUsersInSeasonCommandTests/Constructor_Should.cs
@@ -0,0 +1,47 @@
+﻿using Academy.Commands.Listing;
+using Academy.Core.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace Academy.UnitTest.Commands.Listing.ListUsersInSeasonCommandTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        private Mock<IAcademyFactory> factoryMock;
+        private Mock<IEngine> engineMock;
+
+        [TestInitialize]
+        public void InitializingMocks()
+        {
+            this.factoryMock = new Mock<IAcademyFactory>();
+            this.engineMock = new Mock<IEngine>();
+        }
+        [TestMethod]
+        public void ReturnAListUsersInSeasonCommand_WhenParametersAreCorrect()
+        {
+            //Arrange
+            //Act & Assert
+            Assert.IsNotNull(new ListUsersInSeasonCommand(this.factoryMock.Object, this.engineMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenFactoryIsNull()
+        {
+            //Arrange
+            IAcademyFactory factory = null;
+            //Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new ListUsersInSeasonCommand(factory, this.engineMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenEngineIsNull()
+        {
+            //Arrange
+            IEngine engine = null;
+            //Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new ListUsersInSeasonCommand(this.factoryMock.Object, engine));
+        }
+    }
+}
---
+++ Academy/Academy.UnitTest/Commands/Listing/ListUsersInSeasonCommandTests/Execute_Should.cs
@@ -0,0 +1,41 @@
+﻿using Academy.Commands.Listing;
+using Academy.Core.Contracts;
+using Academy.Framework.Core.Contracts;
+using Academy.Models.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+
+namespace Academy.UnitTest.Commands.Listing.ListUsersInSeasonCommandTests
+{
+    [TestClass]
+   public class Execute_Should
+    {
+        [TestMethod]
+        public void ShouldInvokeListUsersMethord_WhenParametersAreCorrect()
+        {
+            //Arrange
+            var factoryMock = new Mock<IAcademyFactory>();
+            var engineMock = new Mock<IEngine>();
+            var databaseMock = new Mock<IDatabase>();
+            var firstSeason = new Mock<ISeason>();
+            var secondSeason = new Mock<ISeason>();
+
+            var testParameters = new List<string>() {"1"};
+            var seasons = new List<ISeason>();
+            var command = new ListUsersInSeasonCommand(factoryMock.Object, engineMock.Object);
+
+            
+            seasons.Add(firstSeason.Object);
+            seasons.Add(secondSeason.Object);
+            engineMock.SetupGet(x => x.Database).Returns(databaseMock.Object);
+            databaseMock.SetupGet(x => x.Seasons).Returns(seasons);
+
+            //Act
+            command.Execute(testParameters);
+
+            //Assert
+            secondSeason.Verify(x => x.ListUsers(), Times.Once);
+        }
+    }
+}
